5edac81402d1e4715e1a080fcddfd7e0a025372c
Tushar Negi <negitushar81@gmail.com>
2024-08-17 11:20:17 +0530

    remove some page 5 and made footer

--- script.js
+++ script.js
@@ -26,12 +26,13 @@ ScrollTrigger.refresh();
 }
 init();
 
-// var cursor = document.querySelector(".cursor");
-// var main = document.querySelector(".main");
-// main.addEventListener("mousemove", function(dets){
-//     cursor.style.left = dets.x+"px";
-//     cursor.style.top = dets.y+"px";
-// })
+var cursor = document.querySelector(".cursor");
+var main = document.querySelector(".main");
+
+main.addEventListener("mousemove", function(dets){
+    cursor.style.left = dets.x+20+"px";
+    cursor.style.top = dets.y+20+"px";
+})
 
 var tl = gsap.timeline({
     scrollTrigger: {
@@ -85,3 +86,38 @@ var tl3 = gsap.timeline({
 tl3.to(".main", {
     backgroundColor: "#0f0d0d",
 })
+
+var tl3 = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".page1 h1",
+        scroller: ".main",
+        // markers: true,
+        start: "top -650%",
+        end: "top -700%",
+        scrub: 3
+    }
+}) 
+
+tl3.to(".main", {
+    backgroundColor: "#fff",
+})
+
+
+// var boxes = document.querySelectorAll(".box");
+// boxes.forEach(function(elem){
+//     elem.addEventListener("mouseenter", function(){
+//         var att = elem.getAttribute("data-img");
+//         cursor.style.width = "300px"    
+//         cursor.style.height = "250px"
+//         cursor.style.borderRadius = 0
+//         cursor.style.backgroundImage = `url(${att})`
+//     })
+
+//     elem.addEventListener("mouseleave", function(){
+//         var att = elem.getAttribute("data-img");
+//         cursor.style.width = "20px"    
+//         cursor.style.height = "20px"
+//         cursor.style.borderRadius = "50%"
+//         cursor.style.backgroundImage = `none`
+//     })
+// });
